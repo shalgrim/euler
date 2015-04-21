e65ea6d38f5a1446de181d7e5651e4dc3d523f70
shalgrim@hotmail.com <shalgrim@hotmail.com>
2015-04-20 21:45:17 -0700

    set up skeleton for p053

---
+++ p053_combinatoric_selections/p053.py
@@ -0,0 +1,21 @@
+"""
+There are exactly ten ways of selecting three from five, 12345:
+
+123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
+
+In combinatorics, we use the notation, 5 choose 3 = 10.
+
+In general,
+n choose 4 = n!/(r!(n-r)!) ,
+where r <= n, n! = n x (n-1) x ... x 3 x 2 x 1, and 0! = 1.
+
+It is not until n = 23, that a value exceeds one-million:
+23 choose 10 = 1144066.
+
+How many, not necessarily distinct, values of n choose r, for 1 <= n <= 100, are
+greater than one-million?
+"""
+__author__ = 'Scott'
+
+if __name__ == '__main__':
+    raise NotImplementedError
