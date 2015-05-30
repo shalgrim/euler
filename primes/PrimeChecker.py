49ac7b99bbacfef4c34830c58c776137a2cca640
shalgrim@hotmail.com <shalgrim@hotmail.com>
2015-05-30 14:47:19 -0700

    i think it all works now geez git is confusing

--- primes/PrimeChecker.py
+++ primes/PrimeChecker.py
@@ -9,51 +9,48 @@ class PrimeChecker(object):
     _known_primes_sorted = []
     _highest_n_checked = 1
 
-    @classmethod
-    def get_known_primes(cls):
-        return cls._known_primes
+    def __init__(self):
         self._starting_point = 2
+        return
 
-    @classmethod
-    def get_sorted_known_primes(cls):
-        return cls._known_primes_sorted
+    def get_known_primes(self):
+        return self._known_primes
 
-    @classmethod
-    def add_prime(cls, p):
+    def get_sorted_known_primes(self):
+        return self._known_primes_sorted
+
+    def add_prime(self, p):
         assert isinstance(p, int)
         assert p >= 2
-        cls._known_primes.add(p)
-        cls._known_primes_sorted = sorted(cls._known_primes)
-        return cls.get_known_primes()
+        self._known_primes.add(p)
+        self._known_primes_sorted = sorted(self._known_primes)
+        return self.get_known_primes()
 
-    @classmethod
-    def get_highest_n_checked(cls):
-        return cls._highest_n_checked
+    def get_highest_n_checked(self):
+        return self._highest_n_checked
 
-    @classmethod
-    def _set_highest_n_checked(cls, n):
+    def _set_highest_n_checked(self, n):
         assert isinstance(n, int)
-        if cls.get_highest_n_checked() > n:
+        if self.get_highest_n_checked() > n:
             return -1
 
-        cls._highest_n_checked = n
+        self._highest_n_checked = n
     
-        return cls.get_highest_n_checked()
+        return self.get_highest_n_checked()
 
-    @classmethod
-    def is_prime(cls, n):
+    def is_prime(self, n):
         assert isinstance(n, int)
 
         if n < 2:
             return False
 
-        if n in cls.get_known_primes():
+        if n in self.get_known_primes():
             return True
 
-        if n in cls._known_non_primes:
+        if n in self._known_non_primes:
             return False
 
-        return cls._is_prime(n)
+        return self._is_prime(n)
 
     def unknown_generator(self, n):
         """
@@ -67,6 +64,7 @@ class PrimeChecker(object):
             if i not in knowns:
                 yield i
 
+    def _is_prime(self, n):
         """
         Assume n >= 2 and n is int
         """
@@ -97,13 +95,13 @@ class PrimeChecker(object):
 
         return False
 
-    @classmethod
-    def is_truncatable_prime(cls, n):
+
+    def is_truncatable_prime(self, n):
         assert isinstance(n, int)
         if n < 23: return False
         digits = str(n)
-        if int(digits[0]) not in cls.ONE_DIGIT_PRIMES: return False
-        if int(digits[-1]) not in cls.ONE_DIGIT_PRIMES: return False
+        if int(digits[0]) not in self.ONE_DIGIT_PRIMES: return False
+        if int(digits[-1]) not in self.ONE_DIGIT_PRIMES: return False
 
         ints_to_check = set([n])
 
@@ -112,6 +110,6 @@ class PrimeChecker(object):
             ints_to_check.add(int(digits[-i:])) # last i digits
 
         for itc in ints_to_check:
-            if not cls.is_prime(itc): return False
+            if not self.is_prime(itc): return False
 
         return True
